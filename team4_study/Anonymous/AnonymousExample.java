23cc1a5dafb15e328879b1cf78dfc1d97b9cb001
PNT-01 <sjh981217@gmail.com>
2024-06-19 18:37:12 +0900

    0619

---
+++ team4_study/Anonymous/AnonymousExample.java
@@ -0,0 +1,15 @@
+package team4_study;
+
+public class AnonymousExample {
+
+	public static void main(String[] args) {
+		Anonymous anony = new  Anonymous();
+		anony.feild.run();
+		anony.method1();
+		anony.method2(
+				
+		);
+
+	}
+
+}
