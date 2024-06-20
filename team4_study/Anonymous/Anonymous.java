4fe25c2de6be5bfddd94b54b6b5441408d748bb4
PNT-01 <sjh981217@gmail.com>
2024-06-20 16:14:58 +0900

    0620 hw

--- team4_study/Anonymous/Anonymous.java
+++ team4_study/Anonymous/Anonymous.java
@@ -6,12 +6,15 @@ public class Anonymous {
 		public void run() {
 			System.out.println("자전거가 달립니다.");
 		}
+	};
 			
 	void method1() {
 		Vehicle localVar = new Vehicle() {
 			@Override
 			public void run() {
 				System.out.println("승용차가 달립니다.");
+			}
+		};
 		localVar.run();
 	}
 		
